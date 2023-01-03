9f8768eb5128dee061c8eadc065a96aefccd2466
art2282004 <yakimuk.artoum@gmail.com>
2023-01-03 21:33:02 +0300

    first commit

---
+++ scriptpsx.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Muhammad6196/Project-WD/main/Main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/art2282004/scriptpsx/master/scriptpsx.lua"))()
